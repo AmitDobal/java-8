e810b40141d10a3ba6d2c8cc9238a9dab5b1b71e
Amit Dobal <2019pte5188@mnit.ac.in>
2023-12-09 13:37:09 +0530

    Initial commit

---
+++ src/com/learnJava/lambdas/RunnableLambdaExample.java
@@ -0,0 +1,18 @@
+package com.learnJava.lambdas;
+
+public class RunnableLambdaExample {
+
+    public static void main(String[] args) {
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Inside the Runnable 1");
+            }
+        };
+        new Thread(runnable).start();
+
+        new Thread(() -> System.out.println("Inside Runnable 2")).start();
+
+    }
+}
